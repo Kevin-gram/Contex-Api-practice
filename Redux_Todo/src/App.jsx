37fa57a4fcab0fb962732c05aac8ee49d62f3bc2
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-15 19:13:47 +0200

    todo list

---
+++ Redux_Todo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
+    </div>
+  );
+}
+
+export default App;
